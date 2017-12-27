80612c6bc5457803672d6126d0c03f3929bb9012
DigitalJaunt <digitaljaunt@localhost>
2017-12-27 13:29:52 -0500

    Completed solution for Reverse Integer

---
+++ 0007_reverse_integer/0007_reverse_integer_01.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) {
+		const int MAX = 2147483647;
+		const int MIN = -2147483648;
+        bool negative = x < 0;		
+		int answer = 0;
+        while (x)
+		{
+			int digit = x % 10;
+			bool overflow = negative ? answer < ((MIN - digit) / 10) : answer > ((MAX - digit) / 10);
+			if (overflow)
+			{
+				answer = 0;
+				break;
+			}
+			answer = answer * 10 + digit;
+			x /= 10;
+		}
+		return answer;
+    }
+};
