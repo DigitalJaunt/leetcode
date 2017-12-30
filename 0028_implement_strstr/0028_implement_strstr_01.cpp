ebb211111f4757db1a76e469eb81f944473933f3
DigitalJaunt <digitaljaunt@localhost>
2017-12-30 00:10:03 -0500

    Complete solution for Implement strStr()

--- 0028_implement_strstr/0028_implement_strstr_01.cpp
+++ 0028_implement_strstr/0028_implement_strstr_01.cpp
@@ -11,14 +11,18 @@ public:
 			if (needle[0] == haystack[i])
 			{
 				int j=1;
+				// Used to check if the start of the needle is inside 
+				// the current needle candidate that we're searching
+				int nextStartOffset = 0;
 				for (j; j<needle.length(); ++j)
 				{
+					if (!nextStartOffset && haystack[i+j] == needle[0]) nextStartOffset = j;
 					if (needle[j] != haystack[i+j]) break;
 				}
 				if (j == needle.length())
 					return i;
 				else
-					i += j;
+					i += (nextStartOffset > 0) ? nextStartOffset : j;
 			}
 			else
 			{
