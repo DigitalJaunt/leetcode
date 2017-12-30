5d1fd9b6d554f4773c9c0814d54e84d363ba039a
DigitalJaunt <digitaljaunt@localhost>
2017-12-29 22:51:02 -0500

    Possible solution finished for Implement strStr()

--- 0028_implement_strstr/0028_implement_strstr_01.cpp
+++ 0028_implement_strstr/0028_implement_strstr_01.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+		if (needle.length() > haystack.length())
+			return -1;
+		int i = 0;
+		while (i <= haystack.length()-needle.length())
+		{
+			if (needle[0] == haystack[i])
+			{
+				int j=1;
+				for (j; j<needle.length(); ++j)
+				{
+					if (needle[j] != haystack[i+j]) break;
+				}
+				if (j == needle.length())
+					return i;
+				else
+					i += needle.length();
+			}
+			else
+			{
+				++i;
+			}
+		}
+		// If we got here, then either needle wasn't found
+		// or haystack and needle are both empty
+		return (haystack.empty()) ? 0 : -1;
     }
 };
