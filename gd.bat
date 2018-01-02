91944243131259e31e48bf7b7864ae30d2cdc9f1
user <user@user.com>
2018-01-02 17:17:55 -0500

    Added batch script to make files. Added skeleton for Count And Say

---
+++ gd.bat
@@ -0,0 +1,10 @@
+@echo off
+set /p name="Enter directory name: "
+if %name%="" exit
+set fulldir=%cd%\%name%
+mkdir %fulldir%
+set textfile=%fulldir%\%name%.txt
+set cppfile=%fulldir%\%name%_01.cpp
+type nul >%textfile%
+type nul >%cppfile%
+pause
