9bbcaa1e1115cfa00cdae740f19c52d3b49f971e
user <user@user.com>
2018-01-05 15:43:37 -0500

    Added solution for Maximum Subarray

--- 0053_maximum_subarray/0053_maximum_subarray_01.cpp
+++ 0053_maximum_subarray/0053_maximum_subarray_01.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+		if (nums.empty())
+			return INT_MIN;
+		int maxsum = nums[0];
+		int cursum = nums[0];
+		for (int i=1; i<nums.size(); ++i)
+		{
+			cursum += nums[i];
+			if (nums[i] > cursum)
+				cursum = nums[i];
+			if (cursum > maxsum)
+				maxsum = cursum;
+		}
+		return maxsum;
     }
 };
