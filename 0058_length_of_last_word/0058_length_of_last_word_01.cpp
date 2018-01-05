48ce579f1e2cd635f91296075ac157b08d158f85
user <user@user.com>
2018-01-05 17:45:31 -0500

    Completed Length of Last Word

--- 0058_length_of_last_word/0058_length_of_last_word_01.cpp
+++ 0058_length_of_last_word/0058_length_of_last_word_01.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
+		int result = 0;
+		for (int i=s.length()-1; i>=0; --i)
+		{
+			if (s[i] == ' ')
+			{
+				if (result) break;
+			}
+			else
+			{
+				++result;
+			}
+		}
+		return result;
     }
 };
