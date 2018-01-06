6762666e9af7d87843d346e564841bc228760fb2
DigitalJaunt <digitaljaunt@localhost>
2018-01-06 17:28:15 -0500

    Completed Sqrt(x)

--- 0069_sqrtx/0069_sqrtx_01.cpp
+++ 0069_sqrtx/0069_sqrtx_01.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+		if (x == 0 || x == 1) return x;
+        int i=1;
+		for (i; i<x; ++i)
+		{
+			if ((x/i) < i) break;
+		}
+		return i-1;
     }
 };
